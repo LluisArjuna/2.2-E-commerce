6156c423a43459e6ab2b91087d3facf34ea83032
LluisArjuna <lluisarjunav@gmail.com>
2025-12-18 10:30:30 +0100

    Exercici 1: Afegir productes al carretó

--- js/shop.js
+++ js/shop.js
@@ -64,50 +64,48 @@ const products = [
     }
 ]
 
-// => Reminder, it's extremely important that you debug your code. 
-// ** It will save you a lot of time and frustration!
-// ** You'll understand the code better than with console.log(), and you'll also find errors faster. 
-// ** Don't hesitate to seek help from your peers or your mentor if you still struggle with debugging.
-
-// Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
-const cart = [];
-
-const total = 0;
-
 // Exercise 1
-const buy = (id) => {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cart array
+const buy = (id) => { 
+    const cartItem = cart.find(product => product.id == id);
+    if (cartItem == null) {
+        const product = products.find(product => product.id == id);
+        cart.push({...product, quantity: 1});
+    } else {
+        cartItem.quantity += 1;
+    }
 }
 
 // Exercise 2
 const cleanCart = () =>  {
-
 }
 
 // Exercise 3
 const calculateTotal = () =>  {
-    // Calculate total price of the cart using the "cartList" array
 }
 
 // Exercise 4
 const applyPromotionsCart = () =>  {
-    // Apply promotions to each item in the array "cart"
 }
 
 // Exercise 5
-const printCart = () => {
-    // Fill the shopping cart modal manipulating the shopping cart dom
+const printCart = () => {	
 }
-
-
-// ** Nivell II **
 
 // Exercise 7
 const removeFromCart = (id) => {
-
 }
 
 const open_modal = () =>  {
     printCart();
 }
+
+
+let cart = JSON.parse(localStorage.getItem("cartt")) || [];
+
+const saveCart = (cart) => localStorage.setItem("cartt", JSON.stringify(cart));
+
+document.querySelectorAll("button.add-to-cart").forEach(button => {
+    button.addEventListener("click", () => {
+        buy(button.dataset.productId);
+    })
+});
